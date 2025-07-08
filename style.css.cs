55e704dea27468be7aba2b19f33e1995c383d0fe
nubematrix <royalhprasad@gmail.com>
2025-07-08 18:20:25 +0530

    Add files via upload

---
+++ style.css.cs
@@ -0,0 +1,62 @@
+body {
+  margin: 0;
+  font-family: sans-serif;
+  background-color: #f9f9f9;
+}
+
+header {
+  background: #000;
+  color: #fff;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav a {
+  color: #fff;
+  margin-left: 15px;
+  text-decoration: none;
+}
+
+.banner {
+  background: url('https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?auto=format&fit=crop&w=1470&q=80') no-repeat center center;
+  background-size: cover;
+  height: 300px;
+  text-align: center;
+  color: white;
+  padding-top: 100px;
+}
+
+.banner h2 {
+  font-size: 2em;
+}
+
+.banner button {
+  margin-top: 20px;
+  padding: 10px 20px;
+  font-size: 1em;
+}
+
+.categories {
+  display: flex;
+  justify-content: space-around;
+  padding: 30px 0;
+  background: #fff;
+}
+
+.card {
+  background: #eee;
+  padding: 30px;
+  width: 150px;
+  text-align: center;
+  font-weight: bold;
+  border-radius: 10px;
+}
+
+footer {
+  background: #111;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
